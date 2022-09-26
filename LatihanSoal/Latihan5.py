5d3cfe02f0fbbf8c98483d125ea6c6849be17eaa
reighpuy <daffykhadaffy18@gmail.com>
2022-09-26 14:04:00 +0700

    Diedit

--- LatihanSoal/Latihan5.py
+++ LatihanSoal/Latihan5.py
@@ -10,8 +10,7 @@ Nim anda adalah <outputnim1> nilai UTS anda <outpututs1> nilai UTS anda <outputu
 
 ulang=2
 for i in range(ulang):
- print ("Data Ke " + str(i+1))
- NIM = input("Masukkan NIM anda : ")
+ NIM = input(f"({str(i+1)}/2). Masukkan NIM anda : ")
  UTS = int(input("Masukkan Nilai UTS anda: "))
  UAS = int(input("Masukkan Nilai UAS: "))
  print("NIM anda adalah %s nilai UTS anda %i nilai UTS anda %i" % (NIM,UTS,UAS))
